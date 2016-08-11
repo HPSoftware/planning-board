46ecc89727482831882b657f4e17c46a49eedcdb
Lilo <lilo@hp.com>
2016-08-11 15:33:38 +0300

    *demo page* added support for multiple configurations (layouts & datasets) added initial structure and ui for datasets. each configuration has a layout and 2 data sets. fixed some page layouting issues caused by the board directive.

--- demo.js
+++ demo.js
@@ -1,59 +1,113 @@
 angular.module('planningBoardDemo', ['platform-board']).controller('demoCtrl', function($scope) {
 	'use strict';
 
-	$scope.canMoveMyItem = function(id) {
+	this.canMoveMyItem = function(id) {
 		console.log('canMoveItem ' + id);
 	}
-	$scope.moveMyItem = function(id) {
+	this.moveMyItem = function(id) {
 		console.log('moveItem ' + id);
 	}
 
-	$scope.layout = {
-		columnDefinition: {
-			field: 'status',
-			label: 'Status',
-			values: [
-			{
-				value: 'new',
-				label: 'New'
+	this.configurations = {
+		'Statuses': {
+			layout: {
+			columnDefinition: {
+				field: 'status',
+				label: 'Status',
+				values: [
+					{
+						value: 'new',
+						label: 'New'
+					},
+					{
+						value: 'open',
+						label: 'Open'
+					},
+					{
+						value: 'fixed',
+						label: 'Fixed'
+					},
+					{
+						value: 'closed',
+						label: 'Closed'
+					}
+				]
+			}
+		},
+			dataSets: [
+				{
+
+				}
+			]
+
+		},
+		'Week Planner': {
+			layout: {
+				columnDefinition: {
+					field: 'day',
+					label: 'Day of week',
+					values: [
+						{
+							value: 'mon',
+							label: 'Monday'
+						},
+						{
+							value: 'tue',
+							label: 'Tuesday'
+						},
+						{
+							value: 'wed',
+							label: 'Wednesday'
+						},
+						{
+							value: 'thu',
+							label: 'Thursday'
+						},
+						{
+							value: 'fri',
+							label: 'Friday'
+						},
+						{
+							value: 'sat',
+							label: 'Saturday'
+						},
+						{
+							value: 'sun',
+							label: 'Sunday'
+						}
+					]
+				}
 			},
-			{
-				value: 'open',
-				label: 'Open'
-			},
-			{
-				value: 'fixed',
-				label: 'Fixed'
-			},
-			{
-				value: 'closed',
-				label: 'Closed'
-			},
+			dataSets: [
+				{
+
+				}
 			]
 		}
 	};
+	this.selectedConfiguration = this.configurations['Week Planner'];
 
 	/*
- export class BoardLayout {
- columnDefinition:AxisDefinition;
- laneDefinition:AxisDefinition;
- }
+	 export class BoardLayout {
+	 columnDefinition:AxisDefinition;
+	 laneDefinition:AxisDefinition;
+	 }
 
- export class AxisDefinition {
- name:string;
- label:string;
- values:BoardCell[];
- field:string;
- }
+	 export class AxisDefinition {
+	 name:string;
+	 label:string;
+	 values:BoardCell[];
+	 field:string;
+	 }
 
- export class BoardCell {
- value:string;
- label:string;
- isCollapsed:boolean;
- customData:any;
- }
+	 export class BoardCell {
+	 value:string;
+	 label:string;
+	 isCollapsed:boolean;
+	 customData:any;
+	 }
 
-*/
+	 */
 
 
 });
